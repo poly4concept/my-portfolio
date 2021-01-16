969a8f94988703f70533bd54d12d982d03cb5b7d
Alhazan Mubarak <olajuwonmubarak355@gmail.com>
2021-01-16 17:40:01 +0100

    added the hover effect on the project div

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+/*nav-section*/
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.nav-container');
 
+
+
+/*nav-section*/
 open.addEventListener('click', ()=> container.classList.add('show-nav'));
 close.addEventListener('click', ()=> container.classList.remove('show-nav'));
+

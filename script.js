885aec162af9bee95ec26d89ae8b99dbb3b0cc85
Alhazan Mubarak <olajuwonmubarak355@gmail.com>
2021-01-17 11:26:14 +0100

    added scrolling animation effect

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+/*general*/
+const boxes = document.querySelectorAll(".box");
+
 /*nav-section*/
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.nav-container');
 
+
+/*general*/
+window.addEventListener('scroll', checkBox);
+checkBox();
+function checkBox(){
+    const triggerpoint = window.innerHeight/5 * 4;
+    boxes.forEach(box =>{
+        const boxtop = box.getBoundingClientRect().top;
+        if(boxtop<triggerpoint){
+            box.classList.add('show');
+        }else{
+            box.classList.remove('show');
+        }
+    })
+}
 
 
 /*nav-section*/

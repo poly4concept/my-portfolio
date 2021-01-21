e7111ccc3bde5cef07a19c13858eb591a4a3ef14
Alhazan Mubarak <olajuwonmubarak355@gmail.com>
2021-01-21 12:04:34 +0100

    completed the contact-section

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.nav-container');
 
+/*contact-section*/
+const labels = document.querySelectorAll('.form-group label');
+
+
 
 /*general*/
 window.addEventListener('scroll', checkBox);
@@ -27,3 +31,11 @@ function checkBox(){
 open.addEventListener('click', ()=> container.classList.add('show-nav'));
 close.addEventListener('click', ()=> container.classList.remove('show-nav'));
 
+
+/*contact-section*/
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+            .split('')
+            .map((letter, idx)=> `<span style='transition-delay: ${idx * 50}ms;'>${letter}</span>`)
+            .join('');
+});

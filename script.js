8a3a9e648a4e108b0b6d7a0dad9de63287f3d871
Alhazan Mubarak <olajuwonmubarak355@gmail.com>
2021-02-03 11:54:19 +0100

    fix the bug in the navbar

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 /*general*/
 const boxes = document.querySelectorAll(".box");
 
-/*nav-section*/
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const container = document.querySelector('.nav-container');
-
 /*contact-section*/
 const labels = document.querySelectorAll('.form-group label');
 
@@ -25,11 +20,6 @@ function checkBox(){
         }
     })
 }
-
-
-/*nav-section*/
-open.addEventListener('click', ()=> container.classList.add('show-nav'));
-close.addEventListener('click', ()=> container.classList.remove('show-nav'));
 
 
 /*contact-section*/
